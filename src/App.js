cc3d268e4e36fda4b082fb499d600bb00fe25ebf
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2023-10-17 15:52:29 +0300

    [need to fix] add Blender graphic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import {Canvas} from "@react-three/fiber";
+import ModelViewer from "./ModelViewer";
 
 function App() {
   return (
     <div className="App">
+
       <Canvas>
+          <ModelViewer scale="40" modelPath={"Monkey.glb"} />
+
           <mesh position={[4, 1, 0]}>
               <boxGeometry/>
               <meshStandardMaterial/>
